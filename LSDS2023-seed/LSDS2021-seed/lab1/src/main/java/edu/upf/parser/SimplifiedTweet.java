03019feb467f2073627f10ece6bf8dc3c1665ce5
Moritz Böhm <ge96noc@mytum.de>
2023-01-25 11:58:23 +0100

    Simplified Tweet position updated

--- LSDS2023-seed/LSDS2021-seed/lab1/src/main/java/edu/upf/parser/SimplifiedTweet.java
+++ LSDS2023-seed/LSDS2021-seed/lab1/src/main/java/edu/upf/parser/SimplifiedTweet.java
@@ -1,44 +1,52 @@
-package edu.upf.parser;
-
-import java.util.Optional;
+package main.java.edu.upf.parser;
 
 public class SimplifiedTweet {
 
-  // All classes use the same instance
-  private static JsonParser parser = new JsonParser();
+    private final long tweetId;  // the id of the tweet (’id’)
+    private final String text;   // the content of the tweet (’text’)
+    private final long userId;   // the user id (’user->id’)
+    private final String userName; // the user name (’user’->’name’)
+    private final String language; // the language of a tweet (’lang’)
+    private final long timestampMs; // seconds from epoch (’timestamp_ms’)
 
 
+    public SimplifiedTweet(long tweetId, String text, long userId, String userName, String language, long timestampMs) {
+            this.tweetId = tweetId;
+            this.text = text;
+            this.userId = userId;
+            this.userName = userName;
+            this.language = language;
+            this.timestampMs = timestampMs;
+        }
 
-  private final long tweetId;			  // the id of the tweet ('id')
-  private final String text;  		      // the content of the tweet ('text')
-  private final long ;			  // the user id ('user->id')
-  private final String userName;		  // the user name ('user'->'name')
-  private final String language;          // the language of a tweet ('lang')
-  private final long timestampMs;		  // seconduserIds from epoch ('timestamp_ms')
+        /**
+        * Returns a {@link SimplifiedTweet} from a JSON String.
+        * If parsing fails, for any reason, return an {@link Optional#empty()}
+        *
+        * @param jsonStr
+        * @return an {@link Optional} of a {@link SimplifiedTweet}
+        */
+    public static Optional<SimplifiedTweet> fromJson(String jsonStr) {
+        try {
+            Gson gson = new Gson();
+            JsonReader reader = new JsonReader(new FileReader(filename));
+            SimplifiedTweet tweet = gson.fromJson(reader, SimplifiedTweet.class);
+            return new Optional<SimplifiedTweet>
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+  
+    }
 
-  public SimplifiedTweet(long tweetId, String text, long userId, String userName,
-                         String language, long timestampMs) {
+    public String getLanguage() {
+        return this.language;
+    }
 
-    // PLACE YOUR CODE HERE!
+    @Override
+    public String toString() {
+        // Overriding how SimplifiedTweets are printed in console or the output file
+        // The following line produces valid JSON as output
+        return new Gson().toJson(this);
+    }
 
-  }
-
-  /**
-   * Returns a {@link SimplifiedTweet} from a JSON String.
-   * If parsing fails, for any reason, return an {@link Optional#empty()}
-   *
-   * @param jsonStr
-   * @return an {@link Optional} of a {@link SimplifiedTweet}
-   */
-  public static Optional<SimplifiedTweet> fromJson(String jsonStr) {
-
-    // PLACE YOUR CODE HERE!
-
-  }
-
-
-  @Override
-  public String toString() {
-    return "";
-  }
-}
+    }
