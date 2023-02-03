32398b30ac507deef146a70cc2cfba07ca1bc467
DESKTOP-O10O8FO\kraus <j.kraus@tum.de>
2023-02-03 17:11:04 +0100

    Fixed/Changed the tests a bit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LSDS2021-seed
 Seed for labs
+
+Extensions
+Added some tests
--- lab1/src/test/java/edu/upf/TwitterFilterTest.java
+++ lab1/src/test/java/edu/upf/TwitterFilterTest.java
@@ -33,14 +33,14 @@ public class TwitterFilterTest
     // Test 1 : has all the elements and returns tweet
 
     @Test
-    public void shouldReturnTweet() throws IOException, Exception
+    public void shouldReturnTweet() throws Exception
     {
-            String hello = "{\"tweetId\":0,\"text\":\"test test\",\"userId\":123,\"userName\":\"test\",\"language\":\"es\",\"timestampMs\":15}";
+            String tweetString = "{\"id\":111,\"text\":\"abc\",\"user\":{\"id\":222,\"name\":\"username\"},\"lang\":\"it\",\"timestamp_ms\":\"333\"}";
             // Give valid Json String
-            Optional<SimplifiedTweet> tweet = SimplifiedTweet.fromJson(hello);
+            Optional<SimplifiedTweet> tweet = SimplifiedTweet.fromJson(tweetString);
             // OK?         
             // assertE
-            SimplifiedTweet tweeter = new SimplifiedTweet(0, "test test", 123, "test", "es", 15);
+            SimplifiedTweet tweeter = new SimplifiedTweet(111, "abc", 222, "username", "it", 333);
             String bye = tweeter.toString();
             String bye2 = tweet.get().toString();
             assertEquals("shouldReturnTweet WORKS!", bye, bye2);
@@ -52,70 +52,46 @@ public class TwitterFilterTest
     @Test
     public void shouldReturnEmptyUser() throws IOException
     {
-        String hello2 = "{\"user\": ,\"id\": 1,\"name\":\"test\",\"text\":\"hello world\",\"lang\":\"es\",\"timestamp_ms\":15}";;
+        String tweetString = "{\"id\":111,\"text\":\"abc\",\"lang\":\"it\",\"timestamp_ms\":\"333\"}";
         // Give valid Json String
-        Optional<SimplifiedTweet> tweet2 = SimplifiedTweet.fromJson(hello2);
+        Optional<SimplifiedTweet> tweet2 = SimplifiedTweet.fromJson(tweetString);
         // OK?         
         // assertE
         assertEquals("shouldReturnEmpty WORKS!", Optional.empty(), tweet2);
     }
 
     @Test
-    public void shouldReturnEmptyId() throws IOException
+    public void shouldReturnWrongJsonString() throws IOException
     {
-        String hello2 = "{\"user\": 1,\"id\": ,\"name\":\"test\",\"text\":\"hello world\",\"lang\":\"es\",\"timestamp_ms\":15}";;
-        // Give valid Json String
+        String hello2 = "{\"user\": 1,\"id\": 1,\"name\":,\"text\":\"hello world\",\"lang\":\"es\",\"timestamp_ms\":15}";
         Optional<SimplifiedTweet> tweet2 = SimplifiedTweet.fromJson(hello2);
         // OK?         
         // assertE
-        assertEquals("shouldReturnEmpty WORKS!", Optional.empty(), tweet2);
+        assertEquals("shouldReturnWrongJsonString WORKS!", Optional.empty(), tweet2);
     }
 
     @Test
-    public void shouldReturnEmptyName() throws IOException
+    public void shouldReturnSomethingMissing() throws IOException
     {
-        String hello2 = "{\"user\": 1,\"id\": 1,\"name\":,\"text\":\"hello world\",\"lang\":\"es\",\"timestamp_ms\":15}";;
+        String hello2 = "{\"user\": 1,\"name\":\"test\",\"text\":,\"lang\":\"es\",\"timestamp_ms\":15}";
         // Give valid Json String
         Optional<SimplifiedTweet> tweet2 = SimplifiedTweet.fromJson(hello2);
         // OK?         
         // assertE
-        assertEquals("shouldReturnEmpty WORKS!", Optional.empty(), tweet2);
+        assertEquals("shouldReturnSomethingMissing WORKS!", Optional.empty(), tweet2);
     }
 
     @Test
-    public void shouldReturnEmptyText() throws IOException
+    public void testSomethingHasTheWrongFormat() throws IOException
     {
-        String hello2 = "{\"user\": 1,\"id\": 1,\"name\":\"test\",\"text\":,\"lang\":\"es\",\"timestamp_ms\":15}";;
+        String tweetString = "{\"id\":\"abc\",\"text\":\"abc\",\"user\":{\"id\":222,\"name\":\"username\"},\"lang\":\"it\",\"timestamp_ms\":\"333\"}";
         // Give valid Json String
-        Optional<SimplifiedTweet> tweet2 = SimplifiedTweet.fromJson(hello2);
-        // OK?         
+        Optional<SimplifiedTweet> tweet2 = SimplifiedTweet.fromJson(tweetString);
+        // OK?
         // assertE
-        assertEquals("shouldReturnEmpty WORKS!", Optional.empty(), tweet2);
+        assertEquals("testSomethingHasTheWrongFormat WORKS!", Optional.empty(), tweet2);
     }
 
-    @Test
-    public void shouldReturnEmptyLang() throws IOException
-    {
-        String hello2 = "{\"user\": 1,\"id\": 1,\"name\":\"test\",\"text\":\"hello world\",\"lang\":,\"timestamp_ms\":15}";;
-        // Give valid Json String
-        Optional<SimplifiedTweet> tweet2 = SimplifiedTweet.fromJson(hello2);
-        // OK?         
-        // assertE
-        assertEquals("shouldReturnEmpty WORKS!", Optional.empty(), tweet2);
-    }
-
-    @Test
-    public void shouldReturnEmptyTms() throws IOException
-    {
-        String hello2 = "{\"user\": 1,\"id\": 1,\"name\":\"test\",\"text\":\"hello world\",\"lang\":\"es\",\"timestamp_ms\":}";;
-        // Give valid Json String
-        Optional<SimplifiedTweet> tweet2 = SimplifiedTweet.fromJson(hello2);
-        // OK?         
-        // assertE
-        assertEquals("shouldReturnEmpty WORKS!", Optional.empty(), tweet2);
-    }
-
-    // Test 3 : testToString()
     
     @Test
     public void testToString() 
